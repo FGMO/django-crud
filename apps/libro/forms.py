2a2a740b1855e7f38b27ed52cba261366120cbcd
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2019-09-20 12:24:50 -0500

    Cambio Autor a vista basada en clases.

--- apps/libro/forms.py
+++ apps/libro/forms.py
@@ -10,3 +10,30 @@ class AutorForm(forms.ModelForm):
             'nombres',
             'descripcion',
         ]
+        labels = {
+            'apellidos': 'Apellidos del Autor',
+            'nombres': 'Nombres del Autor',
+            'descripcion': 'Descripción'
+        }
+        widgets = {
+            'apellidos': forms.TextInput(
+                attrs={
+                    'id': 'nombres',
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese los nombres del autor'
+                }
+            ),
+            'nombres': forms.TextInput(
+                attrs={
+                    'id': 'apellidos',
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese los apellidos del autor'
+                }
+            ),
+            'descripcion': forms.Textarea(
+                attrs={
+                    'id': 'descripcion',
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese una descripción para el autor'}
+            )
+        }
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,57 +1,42 @@
-from django.shortcuts import render, redirect
-from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+from django.views.generic import View, TemplateView, CreateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import AutorForm
 from .models import Autor
 
 # Create your views here.
 
 
-def Home(request):
-    return render(request, 'index.html')
+class Inicio(TemplateView):
+    template_name = 'index.html'
 
 
-def crearAutor(request):
-    if request.method == 'POST':
-        autor_form = AutorForm(request.POST)
-        if autor_form.is_valid():
-            autor_form.save()
-            return redirect('libro:listar_autor')
-    else:
-        autor_form = AutorForm()
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form})
+class CrearAutor(CreateView):
+    model = Autor
+    template_name = 'libro/crear_autor.html'
+    form_class = AutorForm
+    success_url = reverse_lazy('libro:listar_autor')
 
 
-def editarAutor(request, id):
-    autor_form = None
-    error = None
-    try:
-        autor = Autor.objects.get(id=id)
-        if request.method == 'GET':
-            autor_form = AutorForm(instance=autor)
-        else:
-            autor_form = AutorForm(request.POST, instance=autor)
-            if autor_form.is_valid:
-                autor_form.save()
-                return redirect('libro:listar_autor')
-    except ObjectDoesNotExist as e:
-        error = e
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form, 'error': error})
+class ListarAutor(ListView):
+    template_name = 'libro/listar_autor.html'
+    context_object_name = 'autores'
+    queryset = Autor.objects.filter(eliminado=False)
 
-# def eliminarAutor(request, id):
-#     autor = Autor.objects.get(id=id)
-#     autor.delete()
-#     return redirect('libro:listar_autor')
 
-def eliminarAutor(request, id):
-    autor = Autor.objects.get(id=id)
-    if request.method == 'POST':
-        # autor.delete()
-        autor.eliminado = True
-        autor.save()
+class ActualizarAutor(UpdateView):
+    model = Autor
+    template_name = 'libro/crear_autor.html'
+    form_class = AutorForm
+    success_url = reverse_lazy('libro:listar_autor')
+
+
+class EliminarAutor(DeleteView):
+    model = Autor
+    template_name = 'libro/eliminar_autor.html'
+
+    def post(self, request, pk, *args, **kwargs):
+        object = Autor.objects.get(id=pk)
+        object.eliminado = True
+        object.save()
         return redirect('libro:listar_autor')
-    return render(request, 'libro/eliminar_autor.html', {'autor': autor})
-
-def listarAutor(request):
-    # autores = Autor.objects.all()
-    autores = Autor.objects.filter(eliminado = False)
-    return render(request, 'libro/listar_autor.html', {'autores': autores})
